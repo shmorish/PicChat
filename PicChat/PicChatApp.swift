1c628be623cee3493c2ee7692bf49875ba75abf2
Mori062 <01morishi@gmail.com>
2024-02-14 02:35:40 +0900

    Initial Commit

---
+++ PicChat/PicChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicChatApp.swift
+//  PicChat
+//
+//  Created by 森下翔登 on 2024/02/14.
+//
+
+import SwiftUI
+
+@main
+struct PicChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
